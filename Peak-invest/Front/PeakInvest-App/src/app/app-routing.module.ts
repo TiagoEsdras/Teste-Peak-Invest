fb3a5bc2369f683e86129589823cbe979c08eded
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2021-03-30 10:28:16 -0300

    Comecando as telas do app

--- Peak-invest/Front/PeakInvest-App/src/app/app-routing.module.ts
+++ Peak-invest/Front/PeakInvest-App/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './views/cadastro/cadastro.component';
+import { ConsultaComponent } from './views/consulta/consulta.component';
+import { HomeComponent } from './views/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "consulta",
+    component: ConsultaComponent
+  },
+  {
+    path: "cadastro",
+    component: CadastroComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Peak-invest/Front/PeakInvest-App/src/app/app.module.ts
+++ Peak-invest/Front/PeakInvest-App/src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
+import { HomeComponent } from './views/home/home.component';
+import { NavtoolbarComponent } from './components/navtoolbar/navtoolbar.component';
+import { CadastroComponent } from './views/cadastro/cadastro.component';
+import { ConsultaComponent } from './views/consulta/consulta.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavtoolbarComponent,
+    CadastroComponent,
+    ConsultaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
